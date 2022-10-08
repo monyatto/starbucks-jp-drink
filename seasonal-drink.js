1fd84fe9a0932f95810b1823cc23d1851801c715
monyatto <atsuuu.0925@gmail.com>
2022-10-08 17:03:34 +0900

    コメント文などを削除

--- seasonal-drink.js
+++ seasonal-drink.js
@@ -1,40 +1,30 @@
 const DrinkList = require('./lib/drinkList.js')
 
-async function　main(){
-const searchOpt = ["seasonal"]　 // drip espresso frappuccino tea others or
+async function main () {
+  const searchOpt = ['seasonal']
 
-// const hotOrColdOpt = 'hot cold both others' //hot cold both others or
+  const drinkList = new DrinkList(searchOpt)
 
-// const seasonalOpt = 'seasonal regular' //seasonal regular
+  const productName = await drinkList.productName()
+  const catchcopy = await drinkList.catchcopy()
+  const note = await drinkList.note()
 
-// const outputItem = "name catchcopy note" //name catchcopy note
-
-const drinkList = new DrinkList(searchOpt)
-
-
-const productName = await drinkList.productName()
-const catchcopy = await drinkList.catchcopy()
-const note = await drinkList.note()
-
-console.log(
+  console.log(
 `----------------------
 現在の季節限定ドリンク
-----------------------
+----------------------å
 `)
 
-productName.forEach((item, i) => {
-   console.log(
-`${i+1}. ${productName[i]}
+  productName.forEach((item, i) => {
+    console.log(
+`${i + 1}. ${productName[i]}
 
 〜 ${catchcopy[i]} 〜
 
 ${note[i]}
 
-`)});
-
-
-// const catchcopy = new DrinkList(searchOpt).catchcopy()
-// const note = new DrinkList(searchOpt).note()
+`)
+  })
 }
 
 main()
